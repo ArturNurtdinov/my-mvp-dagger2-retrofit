961669d7feea64acdfd817e559262b22fb3fa07a
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-03-09 17:39:21 +0300

    Remove debug logs

--- app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/main/MainPresenter.kt
+++ app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/main/MainPresenter.kt
@@ -1,6 +1,5 @@
 package com.infinitevoid.my_mvp_dagger2_retrofit.ui.main
 
-import android.util.Log
 import com.infinitevoid.my_mvp_dagger2_retrofit.api.ApiServiceInterface
 import com.infinitevoid.my_mvp_dagger2_retrofit.models.UserList
 import com.infinitevoid.my_mvp_dagger2_retrofit.ui.di.DaggerApiComponent
@@ -13,18 +12,19 @@ class MainPresenter : MainContract.Presenter {
     init {
         DaggerApiComponent.create().inject(this)
     }
+
     private val subscriptions = CompositeDisposable()
     lateinit var view: MainContract.View
-    @Inject lateinit var api: ApiServiceInterface
+    @Inject
+    lateinit var api: ApiServiceInterface
 
     override fun loadData() {
-        Log.d("MY_TAG", "Happened!")
         val disposable = api.getUsers("rokano").subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ userList: UserList? ->
                 view.showData(userList!!)
             }, { error ->
-                Log.d("MY_TAG", error.localizedMessage)
+                error.printStackTrace()
             })
 
         subscriptions.add(disposable)
