ad5c1284504c5c83dc45ad292e9451d5c6bb90ec
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-03-10 18:28:01 +0300

    Split factory

--- app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/di/ApiModule.kt
+++ app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/di/ApiModule.kt
@@ -1,21 +1,40 @@
 package com.infinitevoid.my_mvp_dagger2_retrofit.ui.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.infinitevoid.my_mvp_dagger2_retrofit.api.ApiServiceInterface
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class ApiModule {
     @Provides
-    fun provideApi() : ApiServiceInterface {
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.github.com/search/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
             .build()
-            .create(ApiServiceInterface::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): ApiServiceInterface {
+        return retrofit.create(ApiServiceInterface::class.java)
     }
 }
