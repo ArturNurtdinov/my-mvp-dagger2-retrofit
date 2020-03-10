6a0715a5b69bcbcac09c04eda5e68ca77ee1413f
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-03-10 18:27:45 +0300

    Add singleton

--- app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/di/ApiComponent.kt
+++ app/src/main/java/com/infinitevoid/my_mvp_dagger2_retrofit/ui/di/ApiComponent.kt
@@ -2,7 +2,9 @@ package com.infinitevoid.my_mvp_dagger2_retrofit.ui.di
 
 import com.infinitevoid.my_mvp_dagger2_retrofit.ui.main.MainPresenter
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [ApiModule::class])
 interface ApiComponent {
     fun inject(presenter : MainPresenter)
